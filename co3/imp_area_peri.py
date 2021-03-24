1ccd73dd8a8e32c8ddce97008eadfe64acb2205d
ASWANA B KRISHNA <aswanabkrishna@gmail.com>
2021-03-24 14:03:50 +0530

    msg3

---
+++ co3/imp_area_peri.py
@@ -0,0 +1,12 @@
+from graphics import rectangle as r
+from graphics import circle as c
+from graphics.sec_pack import cuboid as cu
+from graphics.sec_pack import sphere as s
+print(c.area(2))
+print(c.perim(2))
+print(r.area(2,2))
+print(r.perim(2,2))
+print(cu.area(2,2,2))
+print(cu.perim(2,2,2))
+print(s.area(2))
+print(s.perim(2))
